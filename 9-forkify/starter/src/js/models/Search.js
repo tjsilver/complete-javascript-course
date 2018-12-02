d4479bfbaafdffdb2c75f6f870ccc9cbfdd35a7c
tjsilver <pyrosomida@gmail.com>
2018-12-02 17:18:50 +0000

    up to L144 complete

--- 9-forkify/starter/src/js/models/Search.js
+++ 9-forkify/starter/src/js/models/Search.js
@@ -4,13 +4,13 @@ export default class Search {
     constructor(query) {
         this.query = query;
     }
-    
+
     async getResults() {
         const key = 'a947760b81605ad312d617da687641b0';
         try {
             const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
             this.result = res.data.recipes;
-            console.log(this.result);
+            //console.log(this.result);
         } catch(error) {
             alert(error);
         }
